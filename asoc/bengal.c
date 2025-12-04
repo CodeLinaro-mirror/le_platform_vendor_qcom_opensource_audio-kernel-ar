7aa8161a31ef639890671fe8167eb70505326a8e
prajole <prajole@qti.qualcomm.com>
2025-12-04 10:48:49 +0530

    Audio-kernel: As per LOST comment updated copyright
    Change-Id: I1fb093c49a17ce1f6cc1fb6da32ee57508f309ed
    Signed-off-by: prajole <prajole@qti.qualcomm.com>

--- asoc/bengal.c
+++ asoc/bengal.c
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
- * Changes from Qualcomm Technologies, Inc. are provided under the following license:
  * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
- * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #include <linux/clk.h>
